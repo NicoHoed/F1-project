0636c874d4c223fb7c41ad7bbc708b6e3c40d6de
nico <nico.hoed@gmail.com>
2024-11-08 20:01:01 +0100

    drivers initialisation

--- src/driver.c
+++ src/driver.c
@@ -13,9 +13,35 @@ void initialize_driver(Driver *driver, const char *name, int number, const char 
 
 // Initialisation de tous les pilotes
 void initialize_all_drivers(Driver drivers[]) {
-    // Exemple de trois pilotes. Ajoute ici tous les pilotes nécessaires.
-    initialize_driver(&drivers[0], "Lewis Hamilton", 44, "Mercedes");
-    initialize_driver(&drivers[1], "Max Verstappen", 1, "Red Bull");
-    initialize_driver(&drivers[2], "Charles Leclerc", 16, "Ferrari");
-    // Ajoute les autres pilotes de manière similaire...
+
+    initialize_driver(&drivers[0], "Max Verstappen", 1, "Red Bull");
+    initialize_driver(&drivers[1], "Sergio Perez", 11, "Red Bull");
+
+    initialize_driver(&drivers[2], "Lewis Hamilton", 44, "Mercedes");
+    initialize_driver(&drivers[3], "George Russel", 63, "Mercedes");
+
+    initialize_driver(&drivers[4], "Charles Leclerc", 16, "Ferrari");
+    initialize_driver(&drivers[5], "Carlos Sainz", 55, "Ferrari");
+
+    initialize_driver(&drivers[6], "Lando Norris", 4, "McLaren");
+    initialize_driver(&drivers[7], "Oscar Piastri", 81, "McLaren");
+
+    initialize_driver(&drivers[8], "Fernando Alonso", 14, "Aston Martin");
+    initialize_driver(&drivers[9], "Lance stroll", 18, "Aston Martin");
+
+    initialize_driver(&drivers[10], "Pierre Gasly", 10, "Alpine");
+    initialize_driver(&drivers[11], "Esteban Ocon", 31, "Alpine");
+
+    initialize_driver(&drivers[10], "Alexander Albon", 23, "Williams");
+    initialize_driver(&drivers[11], "Franco Colapinto", 43, "Williams");
+
+    initialize_driver(&drivers[10], "Liam Lawson", 3, "Racing bulls");
+    initialize_driver(&drivers[11], "Yuki Tsunoda", 22, "Racing bulls");
+
+    initialize_driver(&drivers[10], "Zhou Guanyu", 24, "Kick Sauber");
+    initialize_driver(&drivers[11], "Valtteri Bottas", 77, "Kick Sauber");
+
+    initialize_driver(&drivers[10], "Kevin Magnussen", 20, "Haas");
+    initialize_driver(&drivers[11], "Nico Hulkenberg", 27, "Haas");
+
 }
