adc8c75c3e063dca61ee6dd43265ac1c3de74ed7
nico <nico.hoed@gmail.com>
2024-11-17 19:30:08 +0100

    pilotes et circuits en CSV

--- src/main.c
+++ src/main.c
@@ -7,27 +7,26 @@ int main() {
     int num_tracks = load_tracks("../data/tracks.csv", tracks, MAX_TRACKS);
 
     if (num_tracks > 0) {
-        printf("Circuits chargés : %d\n", num_tracks);
+        printf("Tracks loaded : %d\n", num_tracks);
         for (int i = 0; i < num_tracks; i++) {
-            printf("ID: %d, Nom: %s, Temps min: %d ms, Temps max: %d ms\n",
+            printf("ID: %d, Name: %s, min: %d ms, max: %d ms\n",
                 tracks[i].id, tracks[i].name, tracks[i].min_time_ms, tracks[i].max_time_ms);
         }
     } else {
-        printf("Aucun circuit chargé.\n");
+        printf("0 tracks loaded.\n");
     }
 
-    // Charger les pilotes
     Driver drivers[MAX_DRIVERS];
     int num_drivers = load_drivers("../data/drivers.csv", drivers, MAX_DRIVERS);
 
     if (num_drivers > 0) {
-        printf("\nPilotes chargés : %d\n", num_drivers);
+        printf("\nDrivers loaded : %d\n", num_drivers);
         for (int i = 0; i < num_drivers; i++) {
-            printf("ID: %d, Nom: %s, Équipe: %s, Nationalité: %s\n",
+            printf("ID: %d, Name: %s, Team: %s, Nationality: %s\n",
                 drivers[i].id, drivers[i].name, drivers[i].team, drivers[i].nationality);
         }
     } else {
-        printf("Aucun pilote chargé.\n");
+        printf("0 drivers loaded.\n");
     }
 
     return 0;
